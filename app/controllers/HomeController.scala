61935879b32bbd7e4dd048a46833ad25e9b3985c
jawilliams24 <jwilliams9517@gmail.com>
2020-03-13 15:03:04 +0000

    Web basics

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,7 +7,21 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Sam is a plague carrier"))
   }
+
+  def getGreeting() = Action { implicit request: Request[AnyContent] =>
+    Ok("Hello World!")
+  }
+
+  def redirect() = Action {
+    Redirect("http://www.google.com")
+  }
+
+  def infinite() = Action {
+    Redirect(routes.HomeController.index())
+  }
+
+  def toDo() = TODO
 
 }
